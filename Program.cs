36a9d7e2dd5f12b89c915848b667619c2e145b5a
Hundeyin Oluwadamilola <d.hundeyin@yahoo.com>
2021-10-25 07:21:44 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace movie_theatre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to movie theatre");
+        }
+    }
+}
